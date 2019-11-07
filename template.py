3f1800ee4f4d1ff68a868a3e2289c7f58e15035c
RaynorChavez <raynorkirksonchavez@gmail.com>
2019-11-08 02:12:22 +0800

    Add files via upload
    Better template file

--- template.py
+++ template.py
@@ -1,46 +1,66 @@
-import pygame
+#Jump Platform game
+import pygame as pg
 import random
-import pyglet
+import pyglet as pyg
 import os
+from settings import *
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+class Game:
+	def __init__(self):
+		#initialize game window
+		pg.init()
+		pg.mixer.init()
+		self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+		pg.display.set_caption(TITLE)
+		self.clock = pg.time.Clock()
+		self.running = True
 
-#define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+	def new(self):
+		#start a new game
+		self.all_sprites = pg.sprite.Group()
+		#self.player = Player()
+		#self.all_sprites.add(self.player)
+		self.run()
 
-#initialize pygame and create window
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Nightmare in DCS")
-clock = pygame.time.Clock()
+	def run(self):
+		#game Loop
+		self.playing = True
+		while self.playing:
+			self.clock.tick(FPS)
+			self.events()
+			self.update()
+			self.draw()
 
-#Sprite groups
-all_sprites = pygame.sprite.Group()
+	def update(self):
+		#Game Loop Update
+		self.all_sprites.update()
 
-#Game Loop
-running = True
-while running:
-	clock.tick(FPS)
+	def events(self):
+		# Game Loop Events
+		for event in pg.event.get(): 
+			#Terminate the program
+			if event.type == pg.QUIT: 
+				self.playing = False
+				self.running = False
 
-	#-----Events-----
-	for event in pygame.event.get(): 
-		#Terminate the program
-		if event.type == pygame.QUIT: 
-			running = False
+	def draw(self):
+		#Game Loop draw
+		self.screen.fill(BLACK)
+		self.all_sprites.draw(self.screen)
+		pg.display.flip() #Flips newly rendered frame to the viewer
 
-	#-----Updating-----
-	all_sprites.update()
+	def show_start_screen(self):
+		# Game splash/start screen
+		pass
 
-	#-----Draw Section-----
-	screen.fill(BLACK)
-	all_sprites.draw(screen)
-	pygame.display.flip() #Flips newly rendered frame to the viewer
+	def show_go_screen(self):
+		#game over/continue
+		pass
 
-pygame.quit()
+g = Game()
+g.show_start_screen()
+while g.running:
+	g.new()
+	g.show_go_screen()
+
+pg.quit()
