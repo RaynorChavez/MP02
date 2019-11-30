70bebeda14fec27306e3c35d5c1330dd48a9b5b3
LCrisostomo <56263741+LCrisostomo@users.noreply.github.com>
2019-11-30 23:00:36 +0800

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -4,45 +4,66 @@ from pyglet.window import mouse
 from pyglet.gl import *
 from os import path
 
-
-
 gameFolder = path.dirname(__file__)
 imageFolder = path.join(gameFolder, "images")
 
-window = pyglet.window.Window(500,500)
-x, y = window.get_location()
-window.set_location(x + 390, y + 50)
 
-class StartGameButton(pyglet.sprite.Sprite):
-	def __init__(self, width,height):
-		img = pyglet.image.load(path.join(imageFolder, "start1.png"))
-		pyglet.sprite.Sprite.__init__(self, img, x = 250 - width/2, y = 100 - height/2)
+class StartButton:
+    def __init__(self):
+        self.button_image = pyglet.image.load(path.join(imageFolder, "startbutton.png"))
+        self.button_image_sprite = pyglet.sprite.Sprite(self.button_image,
+                                                        (window_width / 2) - 100, (window_height / 2) + 50)
 
-class Logo(pyglet.sprite.Sprite):
-	def __init__(self, width,height):
-		img = pyglet.image.load(path.join(imageFolder, "logo.png"))
-		pyglet.sprite.Sprite.__init__(self, img, x = 250 - width/2, y = 300 - height/2)
+    def draw(self):
+        self.draw()
+
+
+class QuitButton:
+    def __init__(self):
+        self.button_image = pyglet.image.load(path.join(imageFolder, "quitbutton.png"))
+        self.button_image_sprite = pyglet.sprite.Sprite(self.button_image,
+                                                        (window_width / 2) - 100, (window_height / 2) - 10)
+
+    def draw(self):
+        self.draw()
+
+
+class Logo:
+    def __init__(self):
+        self.button_image = pyglet.image.load(path.join(imageFolder, "Logo.png"))
+        self.button_image_sprite = pyglet.sprite.Sprite(self.button_image, (window_width / 2) - 100, 10)
+
+    def draw(self):
+        self.draw()
+
+
+background_animation = pyglet.image.load_animation(path.join(imageFolder,'Mainmenu_background_big.gif'))
+background_animation_sprite = pyglet.sprite.Sprite(background_animation)
+
+window_width = background_animation_sprite.width
+window_height = background_animation_sprite.height
+
+window = pyglet.window.Window(window_width, window_height, 'Nightmare at DCS')
+window.set_minimum_size(window_width, window_height)
+
+
+@window.event
+def on_mouse_press(x, y, button, modifier):
+	if button == mouse.LEFT:
+		if 170 <= x <= 370 and 410 <= y <= 460:
+			window.close()
+			engine.StartGame()
+		elif 170 <= x <= 370 and 350 <= y <= 400:
+			window.close()
 
 
 @window.event
 def on_draw():
-	glClearColor(0, 0, 0, 0)
-	glClear(GL_COLOR_BUFFER_BIT)
-	StartGame_Button.draw()
-	Logo.draw()
-
-@window.event
-def on_mouse_press(x,y,button,modifiers):
-	if button == mouse.LEFT:
-		#print('left mouse button clicked')
-		#print(x,y)
-		if (150 <= x <= 350) and (67 <= y <= 133):
-			#print('you clicked start game')
-			window.close()
-			engine.StartGame()
-			#print('end')
+	window.clear()
+	background_animation_sprite.draw()
+	Logo().button_image_sprite.draw()
+	StartButton().button_image_sprite.draw()
+	QuitButton().button_image_sprite.draw()
 
 
-StartGame_Button = StartGameButton(200,65)
-Logo = Logo(200,200)
 pyglet.app.run()
